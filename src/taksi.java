65fd39a2a04e47d3b3ac8fe71aab62eab138c52f
kerim can kaman <kerimcankaman@gmail.com>
2025-01-22 09:32:06 +0300

    taksimetrekayıt

---
+++ src/taksi.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class taksi {
+    public static void main(String[] args) {
+        double km,   tutar,   perkm=2.2 ,  startprice=10;
+
+        Scanner imput=new Scanner(System.in);
+
+        System.out.print("Kilometreyi Giriniz  :  ");
+
+        km=imput.nextDouble();
+
+        tutar = km*perkm+startprice;
+
+        tutar= (tutar<20) ? 20  :  (tutar);
+
+        System.out.print("Net tutar  :  "  +tutar);
+
+
+
+
+
+
+
+
+
+
+    }
+}
