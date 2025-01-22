b812a741b45d2f6dddf90d7ec301a383647ada36
kerim can kaman <kerimcankaman@gmail.com>
2025-01-22 09:56:11 +0300

    dairealan ve cevre kayıt

---
+++ src/dairealan.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+public class dairealan {
+    public static void main(String[] args) {
+
+        Scanner imput = new Scanner(System.in);
+
+        double pi=3.14,radius,alan,cevre ;
+
+
+        System.out.print("Yarıçap Değerini Giriniz  :  ");
+
+        radius = imput.nextDouble();
+        alan = pi*radius*radius;
+        cevre = 2*pi*radius;
+
+        System.out.println("Üçgenin Alanı  :  "+alan);
+        System.out.print("Üçgenin Çevresi  :  "+cevre);
+    }
+}
