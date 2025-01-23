40eb1b1eaec0c0d6b9bdac0d36a62c80760bca43
kerim can kaman <kerimcankaman@gmail.com>
2025-01-23 10:17:49 +0300

    hesap makinesi Switch kayıt

---
+++ src/hesapmakine.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class hesapmakine {
+    public static void main(String[] args) {
+        Scanner imput = new Scanner(System.in);
+
+        int n1, n2, select;
+
+        System.out.print("Birinci Rakamı Giriniz  :  ");
+        n1 = imput.nextInt();
+
+        System.out.print("İkinci rakamı Giriniz  .  ");
+        n2 = imput.nextInt();
+
+        System.out.println("işlem türünü seçiniz: \n" +
+                "1-toplama\n" +
+                "2-çıkartma\n" +
+                "3-çarpma\n" +
+                "4-bölme");
+        select = imput.nextInt();
+
+        switch (select) {
+            case 1:
+                System.out.print("Sonuç  :  " + (n1 + n2));
+                break;
+            case 2:
+                System.out.print("Sonuç : " + (n1 - n2));
+                break;
+            case 3:
+                System.out.print("Sonuç : " + (n1 * n2));
+                break;
+            case 4:
+                if (n2 == 0) {
+                    System.out.print("Hiçbir Sayı 0'a Bölünemez");
+                }else{
+                    System.out.print("Sonuç : " + (n1 / n2));
+                }break;
+            default:
+                    System.out.print("Hatalı Giriş yaptınız");
+        }
+
+
+    }
+}
