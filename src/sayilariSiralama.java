611570c8372d4525e05b967c3c61ce4e0234e682
kerim can kaman <kerimcankaman@gmail.com>
2025-01-24 22:19:13 +0300

    sayı sıralama  kayıt

---
+++ src/sayilariSiralama.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class sayilariSiralama {
+    public static void main(String[] args) {
+
+        Scanner imput = new Scanner(System.in);
+
+        int a,b,c;
+
+        System.out.print("1.Sayıyı Giriniz : ");
+        a = imput.nextInt();
+
+        System.out.print("2.Sayıyı Giriniz :");
+        b=imput.nextInt();
+
+        System.out.print("3.Sayıyı Giriniz : ");
+        c=imput.nextInt();
+
+        if (a<b && a<c){
+            if (b<c){
+                System.out.print("a<b<c");
+            }else {
+                System.out.print("a<c<b");
+            }
+        } else if (b<a && b<c) {
+            if (a<c){
+                System.out.print("b<a<c");
+            }else {
+                System.out.print("b<c<a");
+            }
+
+        }else {
+            if (a<b){
+                System.out.print("c<a<b");
+            }else {
+                System.out.print("c<b<a");
+            }
+        }
+    }
+}
