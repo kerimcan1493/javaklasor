62718f427d1ed03e988803d2aa55aca681e287f9
kerim can kaman <kerimcankaman@gmail.com>
2025-01-26 21:16:48 +0300

    bölünebilen sayılar kayıt

---
+++ src/boluneBilenSayilar.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class boluneBilenSayilar {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int nmbr,adet=0,toplam=0,i;
+
+        System.out.print("Bir Sayı Giriniz : ");
+        nmbr = input.nextInt();
+
+        for (i=1;i<nmbr;i++){
+            if (i%3==0 && i%4==0){
+                adet++;
+                toplam+=i;
+                System.out.println(i);
+            }
+        }System.out.println("Sayıların toplamı  :  "+toplam);
+
+    }
+}
