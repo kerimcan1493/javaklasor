a48f3be5dd325ca1a380636c228b04937cb870d6
kerim can kaman <kerimcankaman@gmail.com>
2025-01-26 22:05:42 +0300

    tek sayı girilene kadar 2'ye ve 4'e bölünebilen sayıları toplayıp yazdıran program

---
+++ src/odev39.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class odev39 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int nmbr,total=0;
+        do {
+            System.out.print("Bir Sayı Giriniz  :  ");
+            nmbr = inp.nextInt();
+            if (nmbr%2==0 && nmbr%4==0){
+                total+=nmbr;
+            }
+
+        }while (nmbr%2==0);
+        System.out.print("Toplam  :  "+total);
+    }
+}
