18190ec1b442adbd151de432310fdb9e4951c1f2
kerim can kaman <kerimcankaman@gmail.com>
2025-01-27 13:15:29 +0300

    Kombinasyaon hesaplama

---
+++ src/homeWork41.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class homeWork41 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a,d,com;
+        int total=1, totaltwo=1,totalthree=1;
+
+        System.out.print("Kombinasyonunu Almak İstediğiniz Sayı :");
+        a = input.nextInt();
+
+        for (int b=1;b<=a;b++){
+            total *=b;
+        }
+        System.out.print("Kaçlı Kombinasyonu Almak İstiyorsun : ");
+        d=input.nextInt();
+
+        for (int c=1; c<=d;c++){
+            totaltwo*=c;
+        }
+        for (int e=1;e<=(a-d);e++){
+            totalthree*=e;
+        }
+        com = total/(totaltwo*totalthree);
+        System.out.print(com);
+    }
+}
