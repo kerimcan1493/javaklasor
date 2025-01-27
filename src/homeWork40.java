038e9a030a576470b56f81a001b7cddda9d0567c
kerim can kaman <kerimcankaman@gmail.com>
2025-01-27 09:58:57 +0300

    4'ün ve 5'in kuvvetlerini yazdıran program kayıt

---
+++ src/homeWork40.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class homeWork40 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int nmbr,i=1,a=1;
+
+        System.out.print("Bir Sayı Giriniz : ");
+        nmbr = input.nextInt();
+
+        System.out.println("4'ün Kuvvetleri : ");
+
+        while (i<=nmbr){
+            System.out.println(i);
+            i*=4;
+        }
+
+        System.out.println("5'in Kuvvetleri : ");
+
+        while (a<=nmbr){
+            System.out.println(a);
+            a*=5;
+        }
+    }
+}
